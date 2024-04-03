e5879220e9e3cd167dce1b198aa1a9169a02ca48
DESKTOP-L4H1LAF\Saad <sishfaq984@gmail.com>
2024-04-03 10:04:57 +0500

    DB Connection WIP

--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,18 @@
-const express = require('express')
+import express from "express"
+import dotenv from 'dotenv'
 
 const app = express()
+dotenv.config()
 const port = process.env.PORT || 3000
+
+//mongoDB_Connection
 
 
 app.get('/', (req, res) => {
-    res.send('Hey there server is working')
+    res.send('workoing')
 })
+
 app.listen(port, () => {
-    console.log(`App is listening on http://localhost${port} port`)
+    console.log(`App is listening on http://localhost:${port} port`)
 })
 
